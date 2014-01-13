e21f10f9a377b4db456deab576849403d3d5b3a1
Karol M. Langner <karol.langner@gmail.com>
2014-01-13 07:49:25 -0500

    Add more to ccCollection stub

--- src/cclib/parser/collection.py
+++ src/cclib/parser/collection.py
@@ -1,3 +1,12 @@
+from cclib.parser import ccopen
+
+
 class ccCollection(object):
 
-    pass
+    def __init__(self, sources):
+        self.sources = sources
+        self.data = [None]*len(self.sources)
+
+    def parse(self):
+        for i,source in enumerate(self.sources):
+            self.data[i] = ccopen(source).parse()
